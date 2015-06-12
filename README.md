fc266faeefaf3198e38b706d9ef2fad038a4927c
TangJiujun <jiujun.tang@gmail.com>
2015-06-12 23:26:40 +0800

    Upload version 1.0

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # hexo-theme-shed
+>A pure and fresh hexo theme that crafted with jQuery and Bootstrap.
+
+### Features
+* Responsive layout
+* List categories and tags in sidebar
+* Highlight code with Highlight.js
+* Delay load Duoshuo comment
+* Delay load Swiftype Search
+
+### Settings
+* Config `_config.yml` and `theme/shed/_config.yml`
+* Replace `theme/shed/source/img/favicon.ico` with your favicon
+* Replace 'swiftype_key' with your swiftype key at `theme/shed/source/js/blog.js` line 8
---
+++ source/js/blog.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+  // Load swiftype
+  $(".st-default-search-input").one('click', function(){
+    (function(w,d,t,u,n,s,e){w['SwiftypeObject']=n;w[n]=w[n]||function(){
+    (w[n].q=w[n].q||[]).push(arguments);};s=d.createElement(t);
+    e=d.getElementsByTagName(t)[0];s.async=1;s.src=u;e.parentNode.insertBefore(s,e);
+    })(window,document,'script','//s.swiftypecdn.com/install/v2/st.js','_st');
+    _st('install','swiftype_key','2.0.0');
+  })
+
+  // Load Comment
+  $('.ds-thread a').one('click', function(){
+    $('.ds-thread').html("");
+    (function() {
+      var ds = document.createElement('script');
+      ds.type = 'text/javascript';ds.async = true;
+      ds.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') + '//static.duoshuo.com/embed.js';
+      ds.charset = 'UTF-8';
+      (document.getElementsByTagName('head')[0]
+       || document.getElementsByTagName('body')[0]).appendChild(ds);
+    })();
+  })
+
+  // Highlight code
+  $('pre code').each(function(i, block) {
+    hljs.highlightBlock(block);
+  });
+});
