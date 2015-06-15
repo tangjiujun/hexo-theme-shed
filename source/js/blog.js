b6984322cf2eef1b6cfff31295d10bada9b81ba2
唐久军 <jiujun.tang@gmail.com>
2015-06-15 21:39:40 +0800

    Update blog.js

--- source/js/blog.js
+++ source/js/blog.js
@@ -1,25 +1,39 @@
-$(document).ready(function() {
-  // Load swiftype
-  $(".st-default-search-input").one('click', function(){
-    (function(w,d,t,u,n,s,e){w['SwiftypeObject']=n;w[n]=w[n]||function(){
+function loadSwiftype(swiftype_key){
+  (function(w,d,t,u,n,s,e){w['SwiftypeObject']=n;w[n]=w[n]||function(){
     (w[n].q=w[n].q||[]).push(arguments);};s=d.createElement(t);
     e=d.getElementsByTagName(t)[0];s.async=1;s.src=u;e.parentNode.insertBefore(s,e);
     })(window,document,'script','//s.swiftypecdn.com/install/v2/st.js','_st');
 
-    _st('install','swiftype_key','2.0.0');
+    _st('install', swiftype_key,'2.0.0');
+}
+
+function loadDuoshuo(){
+  (function() {
+    var ds = document.createElement('script');
+    ds.type = 'text/javascript';ds.async = true;
+    ds.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') + '//static.duoshuo.com/embed.js';
+    ds.charset = 'UTF-8';
+    (document.getElementsByTagName('head')[0]
+     || document.getElementsByTagName('body')[0]).appendChild(ds);
+  })();
+}
+
+$(document).ready(function() {
+  // Ajax load swiftype
+  var swiftype_key = $('#swiftype-key').val();
+  var swiftypeTimeoutId = setTimeout(function(){
+    loadSwiftype(swiftype_key);
+    $(".st-default-search-input").unbind('click');
+  }, 1000);
+  $(".st-default-search-input").one('click', function(){
+    loadSwiftype(swiftype_key);
+    clearTimeout(swiftypeTimeoutId);
   })
 
-  // Load Comment
+  // Ajax load Comment
   $('.ds-thread a').one('click', function(){
     $('.ds-thread').html("");
-    (function() {
-      var ds = document.createElement('script');
-      ds.type = 'text/javascript';ds.async = true;
-      ds.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') + '//static.duoshuo.com/embed.js';
-      ds.charset = 'UTF-8';
-      (document.getElementsByTagName('head')[0]
-       || document.getElementsByTagName('body')[0]).appendChild(ds);
-    })();
+    loadDuoshuo();
   })
 
   // Highlight code
